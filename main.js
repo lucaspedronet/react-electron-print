821e0cf4ef7a7fdd81012c7f94145c5191760425
Lucas Pedro <lucas.lopes@tonolucro.com.br>
2019-09-22 00:54:38 -0300

    arquivo principal config webContents:{ nodeIntegration: true} and getPrinters()

--- main.js
+++ main.js
@@ -6,9 +6,30 @@ var mainWindow = null
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
-    show: true
+    show: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
 
   mainWindow.loadURL(`file://${__dirname}/public/index.html`)
+
+  ipcMain.on("buscar-impressoras", (event, arg) => {
+    console.log(event)
+    console.log(arg)
+    
+    const windPrint = new BrowserWindow({ show: false });
+    var printers = windPrint.webContents.getPrinters()
+
+    windPrint.webContents.on("did-finish-load", () => {
+      windPrint.webContents.print({ silent: true }, (succees, err) => {
+        if(err) throw new err();
+        console.log(succees)
+      })
+    })
+
+    event.returnValue = printers
+    
+  })
   
 })
