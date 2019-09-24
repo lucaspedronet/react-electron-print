833b75224f670d5786245b513ee669c844db55c8
Lucas Pedro Lopes <lucaspedrolbg@gmail.com>
2019-09-24 15:46:14 -0300

    install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Container } from './styles/components'
 import GlobalStyles from './styles/global'
+import ModalPrinter from './components/ModalPrinter'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    <Container>
      <GlobalStyles />
+     <ModalPrinter />
    </Container>
   );
 }
---
+++ src/components/ModalPrinter/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+// import { Container } from './styles';
+
+export default class ModalPrinter extends Component {
+  render() {
+    return (
+      <div>
+        <button>Funcionou</button>
+        <Button>
+          Deu certo!
+        </Button>
+      </div>)
+  }
+}
